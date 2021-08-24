e9560e93c2addb51d3b182ac23edd9020fe4a640
Anwesha Banerjee <anwesha.banerjee@mindtree.com>
2021-08-24 09:04:36 +0000

    commit

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using WishList.Models;
 
 namespace WishList.Data
